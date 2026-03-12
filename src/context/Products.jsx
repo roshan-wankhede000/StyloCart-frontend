e10744ccddbaae14614899ced3bb906c288bde0d
roshan-wankhede000 <roshanwankhde212@gmail.com>
2026-03-12 14:27:33 +0530

    context

--- src/context/Products.jsx
+++ src/context/Products.jsx
@@ -27,6 +27,8 @@ function Products({ children }) {
       if (email) {
         const res = await axios.get(`${back_URL}/getCart`, { withCredentials: true });
         setCart(res.data.cartItems.length);
+      } else {
+        setCart(0);
       }
     } catch (err) {
       console.error("Error fetching cart:", err);
@@ -34,16 +36,23 @@ function Products({ children }) {
   };
 
   useEffect(() => {
-    const params = new URLSearchParams(window.location.search);
-    const email = params.get("email");
 
-    if (email) {
-      localStorage.setItem("email", email);
-      window.history.replaceState({}, document.title, "/");
-    }
+    const init = async () => {
 
-    fetchProducts();
-    getCart();
+      const params = new URLSearchParams(window.location.search);
+      const email = params.get("email");
+
+      if (email) {
+        localStorage.setItem("email", email);
+        window.history.replaceState({}, document.title, "/");
+      }
+
+      await fetchProducts();
+      await getCart();
+    };
+
+    init();
+
   }, []);
 
   return (
